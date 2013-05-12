54345bcb59c21adbaf4ca27809fe4bf7daac59ba
Georgi Valkov <georgi.t.valkov@gmail.com>
2013-05-12 18:08:01 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from sys import exit
 from setuptools import setup
-from distutils.core import Command
+from setuptools.command.test import test as TestCommand
 from os.path import dirname, join as pjoin
 from jenkins_autojobs.version import version
 
@@ -13,9 +14,10 @@ classifiers = (
     'Topic :: Utilities',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 2.7',
-    # 'Programming Language :: Python :: 3.0',
-    # 'Programming Language :: Python :: 3.1',
-    # 'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.0',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
     'License :: OSI Approved :: BSD License',
     #'Development Status :: 1 - Planning',
     #'Development Status :: 2 - Pre-Alpha',
@@ -27,7 +29,7 @@ classifiers = (
 )
 
 requires = (
-    'python-jenkins>=0.2',
+    'jenkins-webapi>=0.1.0',
     'lxml>=2.2.0',
     'PyYAML>=3.10'
 )
@@ -39,38 +41,33 @@ scripts = (
 )
 
 kw = {
-    'name'                 : 'jenkins-autojobs',
-    'version'              : version(),
-
-    'description'          : 'Scripts for automatically creating jenkins jobs from scm branches',
-    'long_description'     : open(pjoin(here, 'README.rst')).read(),
-
-    'author'               : 'Georgi Valkov',
-    'author_email'         : 'georgi.t.valkov@gmail.com',
-
-    'license'              : 'New BSD License',
-
-    'keywords'             : 'jenkins git mercurial svn',
-    'classifiers'          : classifiers,
-
-    'url'                  : 'https://github.com/gvalkov/jenkins-autojobs',
-
-    'packages'             : ('jenkins_autojobs',),
-    'entry_points'         : {'console_scripts'  : scripts},
-    'install_requires'     : requires,
-    'cmdclass'             : {},
-
-    'zip_safe'             : True,
+    'name'             : 'jenkins-autojobs',
+    'version'          : version(),
+    'description'      : 'Scripts for automatically creating jenkins jobs from scm branches',
+    'long_description' : open(pjoin(here, 'README.rst')).read(),
+    'author'           : 'Georgi Valkov',
+    'author_email'     : 'georgi.t.valkov@gmail.com',
+    'license'          : 'New BSD License',
+    'keywords'         : 'jenkins git mercurial svn',
+    'classifiers'      : classifiers,
+    'url'              : 'https://github.com/gvalkov/jenkins-autojobs',
+    'packages'         : ('jenkins_autojobs',),
+    'entry_points'     : {'console_scripts'  : scripts},
+    'install_requires' : requires,
+    'tests_require'    : ['pytest'],
+    'cmdclass'         : {},
+    'zip_safe'         : False,
 }
 
-class PyTest(Command):
-    user_options = []
-    def initialize_options(self): pass
-    def finalize_options(self):   pass
-    def run(self):
-        from subprocess import call
-        errno = call(('py.test', 'tests'))
-        raise SystemExit(errno)
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [] 
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        exit(errno)
 
 kw['cmdclass']['test'] = PyTest
 setup(**kw)
